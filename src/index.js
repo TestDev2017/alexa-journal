4ede3a56c9b97701352bd641dde6a089194d8869
Rajarshi Nigam <raj@nigam.com>
2016-07-29 01:43:37 -0400

    added persistence

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { handler as alexaHandler } from 'alexa-sdk';
 const handlers = {
   'LaunchRequest'() {
     this.emit(':tell', 'launched');
+  'SessionEndedRequest'() {
+    console.log(`saving: ${JSON.stringify(this.attributes)}`);
+    this.emit(':saveState', true);
   },
   'HelloWorldIntent'() {
     this.emit(':tell', 'Hello World!');
@@ -12,6 +15,7 @@ const handlers = {
 export function handler(event, context) {
   const alexa = alexaHandler(event, context);
   alexa.appId = 'amzn1.ask.skill.892358bf-175d-44ca-815e-331f0302d533';
+  alexa.dynamoDBTableName = 'alexaLogEntryUserSettings';
   alexa.registerHandlers(handlers);
   alexa.execute();
 }
